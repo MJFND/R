96e0a1a337cb6c82caf3f481d4d27b71b269d8cc
Junaid Effendi <junaidfnd@gmail.com>
2017-08-06 22:25:06 -0400

    Update Whatsapp_parser.R

--- WhatsAppChatAnalysis/Whatsapp_parser.R
+++ WhatsAppChatAnalysis/Whatsapp_parser.R
@@ -1,156 +1,139 @@
 #=======================================================================================
 #
-# File:        Whatsapp_parser.R
+# File:        Whatsapp_emoji_decorder.R
 # Author:      Junaid Effendi
-# Description: This code parses whatsap group chat files, you may need to customize it.
-# Blog Link: http://www.tensorflowexamples.com/2017/07/analyzing-whatsapp-group-chat-using-r.html
+# Description: This is a whatsapp emoji library, for extracting emojis and custom emojis, 
+#              its count.
+#
 #=======================================================================================
 
 #========================================================
 # Install and Load Packages
 #========================================================
-install.packages(c("stringr", "zoo", "ggplot2", "scales", "dplyr"))
+install.packages("stringr")
 library("stringr")
-library("zoo")
-library("ggplot2")
 library("dplyr")
-library("scales")
 
 #========================================================
-# Loading Data
+# Creating data frame without media
 #========================================================
-all_data = readLines("whatsap_friends.txt")
-head(all_data)
-
-#========================================================
-# Data Wrangling and Cleaning
-#========================================================
-
-#Removing the first message
-all_data = all_data[-1]
-
-#Extracting date and time  
-date_time <- format(strptime(all_data, "%m/%d/%y, %I:%M %p"),"%m/%d/%y, %H:%M")
-head(date_time)
-
-#Extracting date
-date = gsub(",.*$","",date_time) #Fetching all before ","
-
-#Extracting time
-time = gsub("^.*,","",date_time) #Fetching all after ","
-time = str_trim(time) #Removing spaces from both ends
-time 
-
-sender <- 'sender' #Temorary Data
-message <- all_data
-
-#Creating Data Frame
-clean_data = data.frame(date,time,sender,message)
-head(clean_data)
-
-#Extracting sender and message from the data frame
-#Fetching only complete cases
-sender_message = clean_data[complete.cases(clean_data),4] 
-sender_message = gsub("^.*?-","",sender_message)
-sender_message = str_trim(sender_message) 
-
-#Extracting message
-message = gsub("^.*?:","",sender_message) 
-message = str_trim(message) #Removing spaces from both ends
-head(message)
-#Updating the data frame with new message data
-clean_data$message <- as.character(clean_data$message)
-clean_data[complete.cases(clean_data),4] <- message
-
-#Extracting sender names
-sender = gsub("?:.*$","",sender_message) 
-# Removing prefixes and other names from sender "Customized"
-sender = gsub("NED?.*$","",sender)
-sender = str_trim(sender) #Removing spaces from both ends
-head(sender) 
-#Updating the data frame with new sender data
-clean_data$sender <- as.character(clean_data$sender)
-clean_data[complete.cases(clean_data),3] <- sender
-
-#Replacing remaining "sender" values with NA
-clean_data[clean_data=="sender"]<- NA
-
-
-#Using transform function from Zoo Package 
-#Filling NA with previous values
-#Detailed explanation > http://www.tensorflowexamples.com/2017/07/analyzing-whatsapp-group-chat-using-r.html
-clean_data <- transform(clean_data, date = na.locf(date), time = na.locf(time),
-                        sender = na.locf(sender))
-
-
-#This is a custom function, 
-#Use only if you want to remove unknown contacts
-clean_data = subset(clean_data, !grepl("92 334", sender))
-nrow(clean_data)
-
-#Refactorizing 
-clean_data$sender <- as.factor(clean_data$sender)
-
-
-#Exploring Data
-summary(clean_data)
-nrow(clean_data)
-
-summary(clean_data$sender,maxsum = 25)
+#Using same clean_data and removing all media messages 
+# see > https://github.com/MJFND/R/blob/master/WhatsAppChatAnalysis/Whatsapp_parser.R
+clean_data_without_media <- clean_data
+clean_data_without_media[clean_data_without_media=="<Media omitted>"] <- NA
+clean_data_without_media <- clean_data_without_media[complete.cases(clean_data_without_media),]
+head(clean_data_without_media)
 
 #========================================================
-# Feature Engineering
-#========================================================
-#Setting media messages to empty space for length 0
-clean_data[clean_data=="<Media omitted>"]<-""
-#Finding length of each message
-clean_data$message_length <- nchar(clean_data$message)
-#Restting media messages 
-clean_data[clean_data==""]<-"<Media omitted>"
-summary(clean_data$message_length)
-
-#========================================================
-# Data Visualization
+# Emoji Loader Function
 #========================================================
 
-#===== Plot 1 =====#
-ggplot(clean_data, aes(sender))+
-  geom_bar()+
-  ylab("number of messages")
+emoji_loader <- function(filename)
+{
+  emoji_data = read.csv(filename)
+  head(emoji_data)
+  
+  #Adding emoji- to each name to extract patterns with accuracy
+  emoji_data$Names <- paste0("emoji-", emoji_data$Names)
+  emoji_data$Names <-  gsub(" ", "-", emoji_data$Names)
+  
+  
+  #Adding \\ before a regex functional symbol
+  sym <- c("\\$","\\.","\\^","\\*","\\[","\\]","\\?","\\(","\\)")
+  sym_replace <- c("\\\\$","\\\\.","\\\\^","\\\\*","\\\\[","\\\\]","\\\\?","\\\\(","\\\\)")
+  
+  for(i in 1:length(sym)){
+    emoji_data$Symbols <- gsub(sym[i],sym_replace[i],emoji_data$Symbols)
+  }
+  
+  emoji_data$Symbols = str_trim(emoji_data$Symbols) #Removing spaces from both ends
+  
+  return(emoji_data)
+}
 
-#===== Plot 2 =====#
-ggplot(clean_data, aes(sender,message_length))+
-  geom_bar(stat="identity")+
-  ylab("sum of message length")
 
-#===== Plot 3 =====#
-#Mean of the length of each message for each sender
-grouped_mean_length <- aggregate(message_length ~ sender, clean_data, mean)
 
-ggplot(grouped_mean_length, aes(sender,message_length))+
-  geom_bar(stat="identity")+
-  ylab("mean of message length")
+emoji_all = emoji_loader("whatsapp_emoji_all.csv")
+head(emoji_all)
+emoji_all$Symbols
 
-#===== Plot 4 =====#
-#No of records across date
-date_count<-data.frame(table(clean_data$date))
-colnames(date_count) <- c("date", "count")
+emoji_custom = emoji_loader("whatsapp_emoji_custom.csv")
+head(emoji_custom)
+emoji_custom$Symbols
 
-#Scatter plot for the number of messages per given date
-ggplot(date_count, aes(as.Date(date, "%m/%d/%y"),count))+
-  geom_line()+
-  scale_x_date(breaks = date_breaks("1 months"),labels = date_format("%m/%y"))+
-  xlab("date")+
-  ylab("number of messages")
+emoji_human = emoji_loader("whatsapp_emoji_human.csv")
+head(emoji_human)
+emoji_human$Symbols
 
-#===== Plot 5 =====#
-#No of records across date and sender
-date_sender_count<-data.frame(table(clean_data$sender,clean_data$date))
-colnames(date_sender_count) <- c("sender","date", "count")
+# =====================================
 
-#Scatter plot for the number of messages for each sender per given date
-ggplot(date_sender_count, aes(as.Date(date, "%m/%d/%y"), count, color=sender))+
-  geom_line()+
-  scale_x_date(breaks = date_breaks("1 months"),labels = date_format("%m/%y"))+
-  xlab("date")+
-  ylab("number of messages")
+
+#========================================================
+# Human Emoji Function
+#========================================================
+
+#Run this function only if you want to ignore the colors
+#And want to consider all the six colors as same
+emoji_human_color_ignore <- function(data)
+{
+  data$Names <- gsub(":.*$","",data$Names) 
+  
+  return(data)
+}
+
+
+emoji_human <- emoji_human_color_ignore(emoji_human)
+
+
+
+#========================================================
+# Emoji Replacer Function
+#========================================================
+
+#we have to replace each emoji with its name
+emoji_replacer <- function(data)
+{
+  for(i in seq_len(nrow(data))){
+    clean_data_without_media_temp = gsub(data[i,2],data[i,1],clean_data_without_media[,4])
+    clean_data_without_media$message  = gsub(data[i,2],data[i,1],clean_data_without_media$message)
+  }
+  return(clean_data_without_media)
+}
+
+
+clean_data_without_media <- emoji_replacer(emoji_all)
+clean_data_without_media <- emoji_replacer(emoji_custom)
+clean_data_without_media <- emoji_replacer(emoji_human)
+
+# =========================================================
+
+#Creating columns for each emoji with count
+emoji_array <- as.character(emoji_all$Names)
+clean_data_without_media_with_emoji <- clean_data_without_media
+
+for(i in emoji_array){
+  clean_data_without_media_with_emoji[,i] <- str_count(clean_data_without_media$message,i)
+  
+}
+
+summary(clean_data_without_media_with_emoji)
+unused_emoji_index = c(NA)
+#Getting indexes of unused emojis
+for(i in 6:length(clean_data_without_media_with_emoji)){
+  sum = sum(clean_data_without_media_with_emoji[,i])
+  cat(sprintf("%s = %i \n",names(clean_data_without_media_with_emoji)[i],sum))
+  if(sum == 0)
+  {
+    unused_emoji_index <-  c(unused_emoji_index,i)
+  }
+}
+unused_emoji_index <- unused_emoji_index[-1] 
+unused_emoji_index
+length(unused_emoji_index)
+
+#Removing unused emojis
+clean_data_without_media_with_emoji <- clean_data_without_media_with_emoji[,-unused_emoji_index]
+
+#Removing emoji- from column names
+names(clean_data_without_media_with_emoji) <- gsub("^.*?emoji-", "", names(clean_data_without_media_with_emoji))
+
